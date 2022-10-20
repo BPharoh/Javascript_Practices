6515b02092fb09011f64ba0269409da648393a8a
Otulugbu Alexander <s2200742@Alexander-Air.wlan>
2022-10-20 11:08:02 +0300

    Added todo app

---
+++ To_Do_List/main.js
@@ -0,0 +1,92 @@
+const task = document.querySelector("form input");
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+
+window.onload = loadTasks;
+
+
+function loadTasks() {
+  if (localStorage.getItem("tasks") == null) return;
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach(task => { 
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    list.insertBefore(li, list.children[0]);
+  });
+}
+
+function addTask() {
+  
+  // return if task is empty
+  if (task.value === "") {
+    alert("Empty task not allowed please");
+    return false;
+  }
+
+  // add task to local storage
+  localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
+
+  // create list item, add innerHTML and append to ul
+  const li = document.createElement("li");
+  li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
+      <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+      <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+  list.insertBefore(li, list.children[0]);
+  // clear input
+  task.value = "";
+}
+
+function removeTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach(task => {
+    if (task.task === event.parentNode.children[1].value) {
+      // delete task
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  event.parentElement.remove();
+}
+
+
+// let  currentTask = null;
+
+// function getCurrentTask(event) {
+//   currentTask = event.value;
+// }
+
+// edit the task and update local storage
+function editTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  // check if task is empty
+  if (event.value === "") {
+    alert("Task is empty!");
+    event.value = currentTask;
+    return;
+  }
+  // task already exist
+//   tasks.forEach(task => {
+//     if (task.task === event.value) {
+//       alert("Task already exist!");
+//       event.value = currentTask;
+//       return;
+//     }
+//   });
+
+  // update task
+  tasks.forEach(task => {
+    if (task.task === currentTask) {
+      task.task = event.value;
+    }
+  });
+  // update local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+form.addEventListener("submit", addTask);
+
+
+
